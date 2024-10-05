e554da7d081e4653b634c71c98e0fc9df098afae
Ayoubbb1235 <159105060+Ayoubbb1235@users.noreply.github.com>
2024-10-05 06:01:58 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 
 const ul = document.getElementById("menu-list");
 
-
-const menu = document.getElementById("menu-close");
-menu.addEventListener("click", function(){
+const menuu = document.getElementById("menu-btn");
+menuu.addEventListener("click", function(){
     ul.classList.add('active');
 });
 
-
-const menuu = document.getElementById("menu-btn");
-menuu.addEventListener("click", function(){
+const menu = document.getElementById("menu-close");
+menu.addEventListener("click", function(){
     ul.classList.remove('active');
 });
+
+
